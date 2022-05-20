45d398c7514e5a4d2f482f8f9198b3ea44217bc1
HaoNie <903217871@qq.com>
2022-05-20 14:55:58 +0800

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//For use bu Hazel applicatons
+
+#include <stdio.h>
+#include "Hazel/Application.h"
+
+
+//  -------Entry Point------------------------
+#include "Hazel/EntryPoint.h"
+//  ------------------------------------------
